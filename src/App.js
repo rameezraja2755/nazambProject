2d1830f5f665f3d04e6ebe5a9ab0ba109d5ef863
Nazia kiran <naziakiran894@gmail.com>
2022-10-27 03:50:25 -0700

    implemented theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Input from './component/Atoms/input/Input'
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
+import { Typography } from "@mui/material";
+import Button from  './component/Atoms/button/Button'
+import Login from './component/Organisms/logIn/Login'
+
+// import Sidebar from './component/Organisms/Sidebar's
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <ThemeProvider theme={theme}>
+        <Typography
+        variant="body1" color="secondary">hello world</Typography>
+          < Input/>
+          <Button/>
+          <Login />
+          {/* <Sidebar /> */}
+      </ThemeProvider>
     
-    </div>
+
+      </>
   );
 }
 
